849fa62ff65187ccfd886520f3e161810d2375d8
Jonata Nascimento Barbosa <110827553+iamjonn@users.noreply.github.com>
2023-10-17 17:27:43 -0300

    feito footer e funçao para background do header e adicionado area com lista de descarte

--- modules.js/app.js
+++ modules.js/app.js
@@ -1,1 +1,7 @@
-
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('#meu-header');
+    var scrollPosition = window.scrollY;
+  
+    scrollPosition > 0 ? header.classList.add('header-scroll'):header.classList.remove('header-scroll');
+  })
+  
