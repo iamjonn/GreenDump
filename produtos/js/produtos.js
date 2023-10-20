a9f8265c31f6cd8c7299cc971b6af145a43d2e9a
iamjonn <jonata10.jn@gmail.com>
2023-10-20 03:19:03 -0300

    correção caso o usuario digite errado

--- produtos/js/produtos.js
+++ produtos/js/produtos.js
@@ -6,7 +6,7 @@ const descartes = [
     produto: 'celular',
     cliente: 'gabriel almeida',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIG.CSVZ6hd54HZAJwyZ8yBx'
+    foto: 'https://th.bing.com/th/id/OIP.Sp3mt8zkHxj9jYSmfp3TqAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7'
   },
   {
     produto: 'computador',
@@ -18,7 +18,7 @@ const descartes = [
     produto: 'celular',
     cliente: 'clara santino',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIG.CSVZ6hd54HZAJwyZ8yBx'
+    foto: 'https://th.bing.com/th/id/OIP.Sp3mt8zkHxj9jYSmfp3TqAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7'
   },
   {
     produto: 'computador',
@@ -30,25 +30,25 @@ const descartes = [
     produto: 'notebook',
     cliente: 'francisco carlos',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIG.CSVZ6hd54HZAJwyZ8yBx'
+    foto: 'https://th.bing.com/th/id/OIP.BbluIiqSsZXO_hy0W_pahwHaGc?w=189&h=183&c=7&r=0&o=5&pid=1.7'
   },
   {
     produto: 'notebook',
     cliente: 'maria cecilia',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIG.CSVZ6hd54HZAJwyZ8yBx'
+    foto: 'https://th.bing.com/th/id/OIP.BbluIiqSsZXO_hy0W_pahwHaGc?w=189&h=183&c=7&r=0&o=5&pid=1.7'
   },
   {
     produto: 'celular',
     cliente: 'kevin anthony',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIG.CSVZ6hd54HZAJwyZ8yBx'
+    foto: 'https://th.bing.com/th/id/OIP.Sp3mt8zkHxj9jYSmfp3TqAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7'
   },
   {
     produto: 'mouse',
     cliente: 'thiago alcantra',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIG.CSVZ6hd54HZAJwyZ8yBx'
+    foto: 'https://th.bing.com/th/id/OIP.73Fwb9wm-vRia-ZCJN1HVQHaGo?w=243&h=217&c=7&r=0&o=5&pid=1.7'
   },
 ];
 
@@ -83,8 +83,8 @@ function Buscar() {
   </div>`
   ;
   }).join('');
-  telaPrincipal.innerHTML = '';
-  telaPrincipal.insertAdjacentHTML('beforeend', cardsFiltrados, BarraTop());
+  cardsFiltrados.length === 0?
+    alert('O produto digitado não está disponível'): telaPrincipal.innerHTML = '';telaPrincipal.insertAdjacentHTML('beforeend', cardsFiltrados, BarraTop());
 }
 
 
