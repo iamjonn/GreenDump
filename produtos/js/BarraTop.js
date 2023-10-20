94cae394146d4782bf9ab46b97b2443f6a7c7f33
iamjonn <jonata10.jn@gmail.com>
2023-10-20 03:51:27 -0300

    package adicionado

---
+++ produtos/js/BarraTop.js
@@ -0,0 +1,11 @@
+export function BarraTop(){
+  const Barra = document.querySelector('.barra_pesquisa');
+  const Title = document.querySelector('#title');
+
+
+  Title.classList.add('title');
+  Barra.classList.add('barra_pesquisa2');
+}
+
+
+//FUNÇAO QUE LEVA A BARRA DE BUSCA O TOPO
