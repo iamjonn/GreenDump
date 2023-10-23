96bf75207e4244fab47b34cba519107dec202ccf
iamjonn <jonata10.jn@gmail.com>
2023-10-23 02:05:32 -0300

    reformalização da pagina de pesquisa

--- produtos/js/BarraTop.js
+++ produtos/js/BarraTop.js
@@ -1,10 +1,18 @@
 export function BarraTop(){
   const Barra = document.querySelector('.barra_pesquisa');
   const Title = document.querySelector('#title');
+  const ue = document.querySelector('#ue1');
+  const caixaPreta = document.querySelector('.caixa-preta');
 
+
+  
 
   Title.classList.add('title');
   Barra.classList.add('barra_pesquisa2');
+  ue.classList.add('ue2');
+  caixaPreta.classList.add('caixaWhite');
+  
+
 }
 
 
--- produtos/js/descartes.js
+++ produtos/js/descartes.js
@@ -3,48 +3,152 @@ export const descartes = [
     produto: 'celular',
     cliente: 'gabriel almeida',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.Sp3mt8zkHxj9jYSmfp3TqAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/jpFLW'
   },
   {
     produto: 'computador',
     cliente: 'wesley augusto',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.lDubXm1eOaOSvyp5PYWvWQHaFz?w=269&h=211&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/mEXY2'
   },
   {
     produto: 'celular',
     cliente: 'clara santino',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.Sp3mt8zkHxj9jYSmfp3TqAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/jpFLW'
   },
   {
     produto: 'computador',
     cliente: 'pedro augusto',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.lDubXm1eOaOSvyp5PYWvWQHaFz?w=269&h=211&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/mEXY2'
   },
   {
     produto: 'notebook',
     cliente: 'francisco carlos',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.BbluIiqSsZXO_hy0W_pahwHaGc?w=189&h=183&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/chiG1'
   },
   {
     produto: 'notebook',
     cliente: 'maria cecilia',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.BbluIiqSsZXO_hy0W_pahwHaGc?w=189&h=183&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/chiG1'
   },
   {
     produto: 'celular',
     cliente: 'kevin anthony',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.Sp3mt8zkHxj9jYSmfp3TqAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/jpFLW'
   },
   {
     produto: 'mouse',
     cliente: 'thiago alcantra',
     contato: '83 98877-6655',
-    foto: 'https://th.bing.com/th/id/OIP.73Fwb9wm-vRia-ZCJN1HVQHaGo?w=243&h=217&c=7&r=0&o=5&pid=1.7'
+    foto: 'https://encurtador.com.br/ilMR8'
+  },
+
+  //novos
+  {
+    produto: 'mouse',
+    cliente: 'laura dias',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/ilMR8'
+  },
+  {
+    produto: 'celular',
+    cliente: 'gabriel henrique',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/jpFLW'
+  },
+  {
+    produto: 'computador',
+    cliente: 'wendell gabriel',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/mEXY2'
+  },
+  {
+    produto: 'microondas',
+    cliente: 'marcos leonardo',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTunNmi4C0xp8Yn1nCDLZtanT7Ffo2KsnEWAw&usqp=CAU'
+  },
+  {
+    produto: 'televisao',
+    cliente: 'clara vitoria',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7-Xn89m1hmmk2Huvf4CDJo63YuopiW6WuFmJe-q9PA-luxNtnmtnCkCzmfG5olXVYth0&usqp=CAU'
+  },
+  {
+    produto: 'microondas',
+    cliente: 'silvania da silva',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTunNmi4C0xp8Yn1nCDLZtanT7Ffo2KsnEWAw&usqp=CAU'
+  },
+  {
+    produto: 'microondas',
+    cliente: 'jussara pereira',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTunNmi4C0xp8Yn1nCDLZtanT7Ffo2KsnEWAw&usqp=CAU'
+  },
+  {
+    produto: 'microondas',
+    cliente: 'vitoria alcantra',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTunNmi4C0xp8Yn1nCDLZtanT7Ffo2KsnEWAw&usqp=CAU'
+  },
+  {
+    produto: 'televisao',
+    cliente: 'karine mendes',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7-Xn89m1hmmk2Huvf4CDJo63YuopiW6WuFmJe-q9PA-luxNtnmtnCkCzmfG5olXVYth0&usqp=CAU'
+  },
+  {
+    produto: 'televisao',
+    cliente: 'luisa inacia da silva',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7-Xn89m1hmmk2Huvf4CDJo63YuopiW6WuFmJe-q9PA-luxNtnmtnCkCzmfG5olXVYth0&usqp=CAU'
+  },
+  {
+    produto: 'televisao',
+    cliente: 'jaira bolsonara',
+    contato: '83 98877-6655',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7-Xn89m1hmmk2Huvf4CDJo63YuopiW6WuFmJe-q9PA-luxNtnmtnCkCzmfG5olXVYth0&usqp=CAU'
+  },
+  {
+    produto: 'celular',
+    cliente: 'paula tejando',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/jpFLW'
+  },
+  {
+    produto: 'celular',
+    cliente: 'rafael cunha',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/jpFLW'
+  },
+  {
+    produto: 'celular',
+    cliente: 'ray marcele',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/jpFLW'
+  },
+  {
+    produto: 'mouse',
+    cliente: 'sofia ferreira',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/ilMR8'
+  },
+  {
+    produto: 'computador',
+    cliente: 'deolane bezzera',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/mEXY2'
+  },
+  {
+    produto: 'computador',
+    cliente: 'rosangela da silva',
+    contato: '83 98877-6655',
+    foto: 'https://encurtador.com.br/mEXY2'
   },
 ];
